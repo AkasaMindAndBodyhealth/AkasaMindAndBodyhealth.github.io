ba6cc4742bb919435d0b490e229bf6d42f0e9d13
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-11-19 01:46:21 -0700

    Footer work

---
+++ src/Components/StickyFooter/StickyFooter.jsx
@@ -0,0 +1,22 @@
+import 'React', { Component } from 'react';
+
+class StickyFooter extends Component{
+    state = {
+        rightMeow: Date.now()
+    }
+
+
+    render(){
+        return(
+            <div className="stickyFooter">
+                Copyright &copy; {this.state.rightMeow} Akasa Mind and Bodyhealth
+            </div>
+        );
+    }
+
+
+
+
+}
+
+export default StickyFooter;
