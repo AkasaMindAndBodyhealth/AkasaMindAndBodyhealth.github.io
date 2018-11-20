a06d34b34ad482b2ca523fcf0c00834c94396d71
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-11-20 02:12:53 -0700

    Google Fonts

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -9,6 +9,10 @@ class About extends Component{
                 <p>Health is not the absence of disease; it is balance and harmony of functioning in mind and body.  When our thoughts, emotions, body structure, physiology, and relationships are working in unison, we experience what we call health. When something is out of balance, in our minds, bones or joints, organ systems, or relationships, we experience dysfunction in our health. At Akasa we use many different services and therapeutic methods with the intention to help you restore balance and harmony in your life and relationships so you can achieve your personal health goals</p>
                 <h3>Our Therapeutic Modalities</h3>
                 <p>We strive for integration and do not compete with other modalities or practitioners but instead work with them through Holistic services to bring a variety of alternatives to clients everywhere!! We offer many different therapies that help promote holistic health including Massage/Bodywork, Skin Care, Aromatherapy, and Body Alignment!</p>
+                <p>
+                    Disclaimer:
+                    Therapists at Akasa do not diagnose any medical disorder, nor do they prescribe or adjust medications given by your doctor. Each practitioner follows their modalities code of ethics that reflects their certification or training. Therapies at Akasa can be integrated to address health goals from an alternative angle. They are not intended to replace Western medicine but rather compliment them by addressing health concerns. Working together with your health care provider brings progression to each individual’s health goal choices.
+                </p>
             </div>
         );
 
