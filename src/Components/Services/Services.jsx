fe1d7679e704f021f00effe38c8ee3a467c984cd
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-11-28 10:20:22 -0700

    Services Tab

---
+++ src/Components/Services/Services.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class About extends Component{
+
+    render(){
+        return (
+            <div className="container">
+                <h3>Services</h3>
+
+            </div>
+        );
+
+    }
+}
+
+
+
+
+export default About;
