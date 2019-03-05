aeabb6cae808c153aabd682d2aeae267704909df
Schuyler Ankele <schuyler.ankele@gmail.com>
2019-03-05 09:33:45 -0700

    Fix merge conflicts

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -16,33 +16,39 @@ class Navigation extends Component {
         <Link to={'/'}>
            <div className="navbar-brand" href="/"><img src={akasaLogo} alt="Akasa Mind and Bodyhealth"/></div>
         </Link>
-        <div class="navbar-collapse collapse justify-content-center order-2" id="collapsingNavbar">
-          <ul className="nav justify-content-center">
-            <Link to={'/home'} className="nav-item">
+        <div className="navbar-collapse collapse justify-content-center order-2" id="collapsingNavbar">
+          <ul id="pageLinks" className="nav justify-content-center">
+            <Link to={'/'} className="nav-item">
               <li className="nav-link">Home</li>
             </Link>
             <Link to={'/about'} className="nav-item">
               <li className="nav-link">About</li>
             </Link>
-            <Link to={'/service'} className="nav-item">
+            <Link to={'/services'} className="nav-item">
               <li className="nav-link">Services</li>
             </Link>
             <Link to={'/contact'} className="nav-item">
               <li className="nav-link">Contact Us</li>
             </Link>
           </ul>
-          <ul className="nav justify-content-end">
+          <ul id="social" className="nav justify-content-end">
             <li className="nav-item">
-              <a className="nav-link active" href="#">Active</a>
+              <a className="nav-link active" href="https://www.yelp.com/biz/akasa-mind-and-bodyhealth-arvada-2">
+                <i class="fab fa-yelp"></i>
+              </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
+              <a className="nav-link" href="https://www.facebook.com/Akasabodyhealth/">
+                <i class="fab fa-facebook-square"></i>
+              </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
+              <a className="nav-link" href="tel:+17203826832">
+                <i class="fas fa-phone-square"></i>
+              </a>
             </li>
-          </ul> 
-        </div>
+          </ul>
+        </div> 
       </nav> 
     );
   }
