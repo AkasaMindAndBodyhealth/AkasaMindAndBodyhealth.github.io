75d869e989e3176a126f0acea88a873476087731
Schuyler Ankele <schuyler.ankele@gmail.com>
2019-03-05 09:26:04 -0700

    Impoort new components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
 import Navigation from './Components/Navigation';
 import StickyFooter from "./Components/StickyFooter";
+import Home from './Components/Home';
+import About from './Components/About';
+import Services from './Components/Services';
+import Contact from './Components/Contact';
+import logo from './logo.svg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navigation />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+          <div>
+            <Navigation />
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/services" component={Services} />
+            <Route path="/contact" component={Contact} />
+          </div>
+        </Router>
         <StickyFooter/>
       </div>
     );
