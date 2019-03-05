2b478373c2e60ac45e5ce35f69d18afdd2d00c73
Schuyler Ankele <schuyler.ankele@gmail.com>
2019-03-04 23:32:34 -0700

    Navigation tweaks and React Router

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import akasaLogo from '../../images/akasaLogo.png';
-import Link from 'react-router';
+import { BrowserRouter as Link } from 'react-router-dom';
+import './Navigation.scss';
+
 
 class Navigation extends Component {
   constructor() {
@@ -10,20 +12,38 @@ class Navigation extends Component {
 
   render() {
     return (
-        <nav id="main-navigation" className="navbar">
-            <a className="navbar-brand" href="/"><img src={akasaLogo} alt="Akasa Mind and Bodyhealth"/></a>
-            <ul className="nav">
-                <Link to={}>
-                    <li className="nav-link">About</li>
-                </Link>
-                <Link to={}>
-                    <li className="nav-link">Services</li>
-                </Link>
-                <Link to={}>
-                    <li className="nav-link">Contact Us</li>
-                </Link>
-            </ul>
-        </nav>
+      <nav id="main-navigation" className="navbar navbar-expand-md">
+        <Link to={'/'}>
+           <div className="navbar-brand" href="/"><img src={akasaLogo} alt="Akasa Mind and Bodyhealth"/></div>
+        </Link>
+        <div class="navbar-collapse collapse justify-content-center order-2" id="collapsingNavbar">
+          <ul className="nav justify-content-center">
+            <Link to={'/home'} className="nav-item">
+              <li className="nav-link">Home</li>
+            </Link>
+            <Link to={'/about'} className="nav-item">
+              <li className="nav-link">About</li>
+            </Link>
+            <Link to={'/service'} className="nav-item">
+              <li className="nav-link">Services</li>
+            </Link>
+            <Link to={'/contact'} className="nav-item">
+              <li className="nav-link">Contact Us</li>
+            </Link>
+          </ul>
+          <ul className="nav justify-content-end">
+            <li className="nav-item">
+              <a className="nav-link active" href="#">Active</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+          </ul> 
+        </div>
+      </nav> 
     );
   }
 
